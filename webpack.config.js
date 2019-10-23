0fcc34281a707c1e1a7aad4a1dda3afd1c503ca0
unknown <dorultanianosgyorgy@gmail.com>
2019-10-23 18:14:16 +0100

    performance updates

--- webpack.config.js
+++ webpack.config.js
@@ -1,60 +1,71 @@
 const path = require('path');
 const webpack = require('webpack');
 const argv = process.argv;
-const idx_of_mode = argv.indexOf('--mode');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserJSPlugin = require('terser-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
-const config = {
-	name: 'client',
-	mode: process.env.NODE_ENV || 'development',
-	entry: ['./dev/index'],
-	output: {
-		publicPath: './dist',
-		path: path.resolve(__dirname, 'dist'),
-		filename: `tidy-carousel${argv[idx_of_mode ? idx_of_mode + 1 : 0] === 'production' ? '.min.js' : '.js'}`,
-	  library: 'TidyCarousel',
-	  libraryTarget: 'umd',
-		libraryExport: 'default',
-	  globalObject: "typeof self !== 'undefined' ? self : this"
-	},
-	performance: {
-	 maxEntrypointSize: 512000,
-	 maxAssetSize: 512000
- },
- 	plugins: [
-   new webpack.DefinePlugin({ "process.env.NODE_ENV": JSON.stringify("production") }),
-   new webpack.optimize.ModuleConcatenationPlugin(),
-   new webpack.NoEmitOnErrorsPlugin()
- ],
-	module: {
-		rules: [
-			{
-				test: /\.scss/,
-				use: [
-					{loader: "style-loader"},
-					{loader: "css-loader"},
-					{loader: "sass-loader", options: {includePaths: ["./dev/scss", './dev/sass']}}
-				]
-			},
-			{
-		        test: /\.(png|jpg|gif)$/i,
-		        use: [
-		          {
-		            loader: 'url-loader',
-		            options: {
-		              fallback: 'responsive-loader',
-		            },
-		          },
-		        ],
-		    }
-		]
-	},
+module.exports = (env, argv) => {
 
-	devServer: {
-		contentBase: path.join(__dirname, 'dist'),
-		compress: true,
-		writeToDisk: true,
-		port: 8080
-	}
-}
+	return {
+		name: 'client',
+		mode: env === 'production' ? 'production' : 'development',
+		entry: ['./dev/index'],
+		output: {
+			publicPath: '/dist',
+			path: path.resolve(__dirname, 'dist'),
+			pathinfo: false,
+			filename: `tidy-carousel${env === 'production' ? '.min.js' : '.js'}`,
+		  library: 'TidyCarousel',
+		  libraryTarget: 'umd',
+			libraryExport: 'default',
+		  globalObject: "typeof self !== 'undefined' ? self : this"
+		},
+		performance: {
+		 maxEntrypointSize: 512000,
+		 maxAssetSize: 512000
+	 },
+		module: {
+			rules: [
+				{
+	        test: /\.js$/,
+	        include: path.resolve(__dirname, 'dev'),
+	        loader: 'babel-loader',
+	      },
+				{
+					test: /\.scss$/,
+					use: [
+						{loader: MiniCssExtractPlugin.loader},
+						{loader: "css-loader"},
+						{loader: "sass-loader", options: {includePaths: ["./dev/scss", './dev/sass']}},
+					]
+				},
+				{
+			        test: /\.(png|jpg|gif)$/i,
+			        use: [
+			          {
+			            loader: 'url-loader',
+			            options: {
+			              fallback: 'responsive-loader',
+			            },
+			          },
+			        ],
+			    }
+			]
+		},
 
-module.exports = config;
+ 	 	plugins: [
+ 	   // new webpack.DefinePlugin({ "process.env.NODE_ENV": JSON.stringify("production") }),
+ 		 new MiniCssExtractPlugin({
+ 			 filename: env === 'production' ? 'styles.min.css' : 'styles.css'
+ 		 })
+ 	 ],
+
+		devServer: {
+			contentBase: path.join(__dirname, 'dist'),
+			compress: true,
+			writeToDisk: true,
+			port: 8080
+		}
+	};
+};
